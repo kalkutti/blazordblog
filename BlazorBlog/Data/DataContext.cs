a24927f8c85953141416556cb08b6c57c85d1955
kalkutti <kalindu.sembakutti@gmail.com>
2024-03-24 15:09:14 -0400

    entitties

--- BlazorBlog/Data/DataContext.cs
+++ BlazorBlog/Data/DataContext.cs
@@ -1,9 +1,16 @@
+using BlazorBlog.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace BlazorBlog.Data
 {
     public class DataContext : DbContext
     {
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+
+        }
         
+        public DbSet<Dress> Dresses { get; set; }
     }
 }
